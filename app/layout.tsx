d1c22f7708e3a232fffee5bd0ceacca1ed46413c
steinhardt21 <alexandru.enache@protonmail.com>
2023-08-31 14:57:20 +0300

    Added Google Sign in button and created a navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
+'use client'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+import { Navbar } from '@/components';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +19,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <GoogleOAuthProvider clientId='126871510725-6l8pdn1kqio1g30o580oouoic6f56c5o.apps.googleusercontent.com'>
+      <html lang="en">
+        <body className={`${inter.className} bg-white`}>
+          <Navbar />
+          {children}
+        </body>
+      </html>
+    </GoogleOAuthProvider>
   )
 }
